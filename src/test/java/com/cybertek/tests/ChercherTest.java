140435e7a4461c0c82a1ad33480479d744bd173b
Benjamin2955 <ozenbunyamin29@gmail.com>
2022-01-02 09:26:22 -0600

    Initial commit

---
+++ src/test/java/com/cybertek/tests/ChercherTest.java
@@ -0,0 +1,43 @@
+package com.cybertek.tests;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ChercherTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+
+    @AfterMethod
+    public  void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public  void alert (){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
